2d8cb65f3189c4ac4dfd3f567aa6eed70c59f14a
Lai Zhe Hao <lzhao1998@gmail.com>
2017-07-03 09:38:43 +0800

    Created FakeDemo Project

---
+++ test/test_FakeDemo.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "FakeDemo.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_FakeDemo_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement FakeDemo");
+}
